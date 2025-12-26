4ac0bae250892531c546e5dac2ad48409d1d50d7
Tushar Kiran <65966618+kirantushar10@users.noreply.github.com>
2025-12-26 17:27:18 +0100

    Add files via upload

---
+++ finalapp.py
@@ -0,0 +1,63 @@
+import streamlit as st
+from langchain_nvidia_ai_endpoints import NVIDIAEmbeddings, ChatNVIDIA
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_classic.chains  import create_retrieval_chain
+from langchain_core.output_parsers import StrOutputParser
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+import os
+from dotenv import load_dotenv
+import time
+
+load_dotenv()
+
+os.environ["NVIDIA_API_KEY"] = os.getenv("NVIDIA_API_KEY")
+
+llm = ChatNVIDIA(model="meta/llama-3.3-70b-instruct")
+
+def vector_embeddings():
+    if "vectors" not in st.session_state:
+        st.session_state.embeddings = NVIDIAEmbeddings()
+        st.session_state.loader = PyPDFDirectoryLoader("./us_census")
+        st.session_state.docs = st.session_state.loader.load()
+        st.session_state.text_splitter = RecursiveCharacterTextSplitter(chunk_size=700, chunk_overlap=50)
+        st.session_state.final_documents = st.session_state.text_splitter.split_documents(st.session_state.docs[:30])
+        st.session_state.vectors = FAISS.from_documents(st.session_state.final_documents, st.session_state.embeddings)
+
+
+st.title("Nvidia NIM Demo")
+
+prompt = ChatPromptTemplate.from_template(
+    """
+Answer the questions based on the provided context only.
+Please provide the most accurate response based on the question
+<context>
+{context}
+<context>
+Question: {input}
+
+"""
+)
+
+prompt1 = st.text_input("Enter your question from Documents")
+
+if st.button("Document Embedding"):
+    vector_embeddings()
+    st.write("Document Embedding Completed using NVIDIA Embeddings")
+
+if prompt1:
+    document_chain = create_stuff_documents_chain(llm, prompt=prompt)
+    retriever = st.session_state.vectors.as_retriever()
+    retrieval_chain = create_retrieval_chain(retriever, document_chain)
+    start = time.process_time()
+    response = retrieval_chain.invoke({"input": prompt1})
+    print("Time taken: ", time.process_time() - start)
+    st.write(response['answer'])
+
+    with st.expander("See Conversation Details"):
+        st.write("### Prompt Used:")
+        st.write(prompt1)
+        st.write("### Response from LLM:")
+        st.write(response['answer'])
